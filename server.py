893881d54ccc8d5f95942bac40cb228a17587af8
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-08-23 15:48:11 +0800

    Added stores, stores_new, stores_show, warehouses into templates and stores_update view

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import peeweedbevolve
 import peewee as pw
 from flask import Flask, flash, render_template, request, redirect, url_for
-from models import db, Store
+from models import db, Store, Warehouse
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -22,37 +22,77 @@ def migrate():
 def index():
     return render_template('index.html')
 
-@app.route("/store")
-def store():
+@app.route("/stores/new")
+def stores_new():
     stores = Store.select()
-    return render_template('store.html', stores=stores)
+    return render_template('stores_new.html', stores=stores)
+    
 
-@app.route("/store_form", methods=["POST"])
-def create():
-    # Haven't git add . & git commit yet
-
+@app.route("/stores", methods=["POST"])
+def stores_create():
     s = Store(name=request.form['store_name'])
 
     try:
         if s.save():
             flash(f'{s.name} successfully added', 'success')
-            return redirect(url_for('store'))
+            return redirect(url_for('stores_new'))
+
+    except pw.IntegrityError:
+        flash(f'{s.name} already exists', 'danger')
+        return redirect(url_for('stores_new'))
+
+
+@app.route("/stores")
+def stores_index():
+    stores = Store.select()
+    return render_template('stores.html', stores=stores)
+
+
+@app.route("/stores/<store_id>")
+def stores_show(store_id):
+    store = Store.get_by_id(store_id)
+    return render_template('stores_show.html', store=store, store_id=store_id)
+
+
+@app.route("/stores/<store_id>", methods=['POST'])
+def stores_update(store_id):
+    s = Store.get_by_id(store_id)
+    s.name = request.form['store_name']
+
+    try:
+        if s.save():
+            flash(f'{s.name} successfully updated', 'success')
+            return redirect(url_for('stores_show', store_id=store_id))
 
     except:
-        flash(f'{s.name} already exists', 'danger')
-        return redirect(url_for('store'))
+        flash('Please try again', 'danger')
+        return redirect(url_for('stores_show', store_id=store_id))
 
 
-    #  Code below from 'Redirect vs Render' page
+@app.route("/warehouses/new")
+def warehouses_new():
+    warehouses = Warehouse.select()
+    stores = Store.select()
+    return render_template('warehouse.html', warehouses=warehouses , stores=stores)
 
-    # s = Store(name=request.form['store_name'])
 
-    # if s.save():
-    #         flash(f'{s.name} successfully added', 'success')
-    #         return redirect(url_for('store'))
+@app.route("/warehouses", methods=["POST"])
+def warehouses_create():
+
+    try:
+        store = Store.get_by_id(request.form['store_id'])
+        w = Warehouse(location=request.form['location'], store=store)
+        if w.save():
+            flash(f'{w.location} successfully added', 'success')
+            return redirect(url_for('warehouses_new'))
+
+    except pw.IntegrityError:
+        flash(f'{store.name} already assigned a warehouse', 'danger')
+        return redirect(url_for('warehouses_new'))
     
-    # else:
-    #     return render_template('store.html', name=request.form['store_name'])
+    except:
+        flash('Please try again', 'danger')
+        return redirect(url_for('warehouses_new'))
 
 
 if __name__ == '__main__':
