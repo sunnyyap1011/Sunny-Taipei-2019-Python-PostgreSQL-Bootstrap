71b03c2036d0dfd0fd4b34e9e775ac40be4074a5
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-10-05 06:15:38 +0800

    Modified favicon

--- taipei2019_web/__init__.py
+++ taipei2019_web/__init__.py
@@ -16,7 +16,12 @@ def index():
     return render_template('index.html')
 
 
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static', 'image'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static', 'image'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/x-icon')
