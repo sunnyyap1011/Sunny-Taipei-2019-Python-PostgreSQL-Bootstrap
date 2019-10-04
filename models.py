bbdc7aa6cc5b02f52509353d2eeb228ad59b8970
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-10-05 00:08:32 +0800

    Initial Commit

--- models.py
+++ models.py
@@ -19,31 +19,47 @@ class BaseModel(pw.Model):
         else:
             return 0
 
+    # def update(self, *args, **kwargs):
+    #     self.errors = []
+
+    #     if len(self.errors) == 0:
+    #         self.updated_at = datetime.datetime.now()
+    #         return super(BaseModel, self).save(*args, **kwargs)
+    #     else:
+    #         return 0
+
     class Meta:
         database = db
         legacy_table_names = False
 
-class Store(BaseModel):
+class Restaurant(BaseModel):
     name = pw.CharField(unique=True)
+    address = pw.CharField()
+    google_map_link = pw.CharField(null=True)
+    area_code = pw.CharField(null=True)
+    must_try = pw.CharField(null=True)
 
     def validate(self):
-        duplicate_stores = Store.get_or_none(Store.name == self.name)
+        duplicate_name = Restaurant.get_or_none(Restaurant.name == self.name)
+        duplicate_address = Restaurant.get_or_none(Restaurant.address == self.address)
 
-        if duplicate_stores:
-            self.errors.append('Store name not unique')
+        if not self.id and duplicate_name:
+            self.errors.append('Restaurant name not unique')
+        if not self.id and duplicate_address:
+            self.errors.append('Restaurant address already existed')
 
-class Warehouse(BaseModel):
-    store = pw.ForeignKeyField(Store, backref='warehouses', unique=True)
-    location = pw.TextField()
+# class Warehouse(BaseModel):
+#     store = pw.ForeignKeyField(Store, backref='warehouses', unique=True)
+#     location = pw.TextField()
 
-    def validate(self):
-        duplicate_stores_id = Warehouse.get_or_none(Warehouse.store_id == self.store_id)
+#     def validate(self):
+#         duplicate_stores_id = Warehouse.get_or_none(Warehouse.store_id == self.store_id)
 
-        if duplicate_stores_id:
-            self.errors.append('Store id not unique')
+#         if duplicate_stores_id:
+#             self.errors.append('Store id not unique')
 
-class Product(BaseModel):
-    name = pw.CharField(index=True)
-    description = pw.TextField()
-    warehouse = pw.ForeignKeyField(Warehouse, backref='products')
-    color = pw.CharField(null=True)
+# class Product(BaseModel):
+#     name = pw.CharField(index=True)
+#     description = pw.TextField()
+#     warehouse = pw.ForeignKeyField(Warehouse, backref='products')
+#     color = pw.CharField(null=True)
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import peeweedbevolve
 import peewee as pw
 from flask import Flask, flash, render_template, request, redirect, url_for
-from models import db, Store, Warehouse
+from models import db, Restaurant
+import os
+
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.before_request
 def before_request():
@@ -22,92 +24,112 @@ def migrate():
 def index():
     return render_template('index.html')
 
-@app.route("/stores/new")
-def stores_new():
-    stores = Store.select()
-    return render_template('stores_new.html', stores=stores)
+@app.route("/restaurants/new")
+def restaurants_new():
+    restaurants = Restaurant.select()
+    return render_template('restaurants_new.html', restaurants=restaurants)
     
 
-@app.route("/stores", methods=["POST"])
-def stores_create():
-    s = Store(name=request.form['store_name'])
+@app.route("/restaurants", methods=["POST"])
+def restaurants_create():
+    r = Restaurant(name=request.form['restaurant_name'], address=request.form['restaurant_address'], google_map_link=request.form['restaurant_google_map_link'], area_code=request.form['restaurant_area_code'])
 
-    if s.save():
-        flash(f'{s.name} successfully added', 'success')
-        return redirect(url_for('stores_new'))
+    if r.save():
+        flash(f'{r.name} successfully added', 'success')
+        return redirect(url_for('restaurants_new'))
 
     else:
-        flash(f'{s.errors[0]}', 'danger')
-        return redirect(url_for('stores_new'))
-        # return render_template('stores_new.html', errors=s.errors)
+        flash(f'{r.errors[0]}', 'danger')
+        return redirect(url_for('restaurants_new'))
+        # return render_template('restaurants_new.html', errors=s.errors)
 
 
-@app.route("/stores")
-def stores_index():
-    stores = Store.select()
-    return render_template('stores.html', stores=stores)
+@app.route("/restaurants")
+def restaurants_index():
+    restaurants = Restaurant.select().order_by(Restaurant.area_code.asc())
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
-@app.route("/stores/<store_id>")
-def stores_show(store_id):
-    store = Store.get_by_id(store_id)
-    return render_template('stores_show.html', store=store, store_id=store_id)
+@app.route("/restaurants/<restaurant_id>")
+def restaurants_show(restaurant_id):
+    restaurant = Restaurant.get_by_id(restaurant_id)
+    return render_template('restaurants_show.html', restaurant=restaurant, restaurant_id=restaurant_id)
 
 
-@app.route("/stores/<store_id>", methods=['POST'])
-def stores_update(store_id):
-    s = Store.get_by_id(store_id)
-    s.name = request.form['store_name']
+@app.route("/restaurants/<restaurant_id>", methods=['POST'])
+def restaurants_update(restaurant_id):
+    r = Restaurant.get_by_id(restaurant_id)
+    # r.name = request.form['restaurant_name']
+
+    if r:
+        r.must_try = request.form['restaurant_must_try']
+
+        try:
+            if r.save():
+                flash(f"{r.name}'s Must Try successfully updated", 'success')
+                return redirect(url_for('restaurants_show', restaurant_id=restaurant_id))
+
+        except:
+            flash('Please try again', 'danger')
+            return redirect(url_for('restaurants_show', restaurant_id=restaurant_id))
+
+    else:
+        flash('Something wrong', 'danger')
+        return redirect(url_for('restaurants_show', restaurant_id=restaurant_id))
+
+
+@app.route("/restaurants/<restaurant_id>/delete", methods=['POST'])
+def restaurants_destroy(restaurant_id):
+    r = Restaurant.get_by_id(restaurant_id)
 
     try:
-        if s.save():
-            flash(f'{s.name} successfully updated', 'success')
-            return redirect(url_for('stores_show', store_id=store_id))
-
-    except:
-        flash('Please try again', 'danger')
-        return redirect(url_for('stores_show', store_id=store_id))
-
-
-@app.route("/stores/<store_id>/delete", methods=['POST'])
-def stores_destroy(store_id):
-    s = Store.get_by_id(store_id)
-
-    try:
-        if s.delete_instance():
+        if r.delete_instance():
             flash('Successfully deleted', 'success')
-            return redirect(url_for('stores_index'))
+            return redirect(url_for('restaurants_index'))
 
     except:
         flash('Please try again', 'danger')
-        return redirect(url_for('stores_index'))
+        return redirect(url_for('restaurants_index'))
 
 
-@app.route("/warehouses")
-def warehouses_index():
-    warehouses = Warehouse.select()
-    return render_template('warehouses.html', warehouses=warehouses)
+@app.route("/restaurants/search", methods=['POST'])
+def restaurants_search():
+    x = request.form['search_input']
 
+    restaurants = Restaurant.select().where(Restaurant.name.contains(x))
 
-@app.route("/warehouses/new")
-def warehouses_new():
-    warehouses = Warehouse.select()
-    stores = Store.select()
-    return render_template('warehouses_new.html', warehouses=warehouses , stores=stores)
-
-
-@app.route("/warehouses", methods=["POST"])
-def warehouses_create():
-
-    store = Store.get_by_id(request.form['store_id'])
-    w = Warehouse(location=request.form['location'], store=store)
-    if w.save():
-        flash(f'{w.location} successfully added', 'success')
-        return redirect(url_for('warehouses_new'))
-
+    if restaurants: 
+        return render_template('restaurants_search.html', restaurants=restaurants)
     else:
-        flash(f'{w.errors[0]}', 'danger')
-        return redirect(url_for('warehouses_new'))
+        flash('Sorry, NO restaurant match your search. Below are the list of restaurants available.', 'danger')
+        return redirect(url_for('restaurants_index'))
+
+
+# @app.route("/warehouses")
+# def warehouses_index():
+#     warehouses = Warehouse.select()
+#     return render_template('warehouses.html', warehouses=warehouses)
+
+
+# @app.route("/warehouses/new")
+# def warehouses_new():
+#     warehouses = Warehouse.select()
+#     stores = Store.select()
+#     return render_template('warehouses_new.html', warehouses=warehouses , stores=stores)
+
+
+# @app.route("/warehouses", methods=["POST"])
+# def warehouses_create():
+
+#     store = Store.get_by_id(request.form['store_id'])
+#     w = Warehouse(location=request.form['location'], store=store)
+#     if w.save():
+#         flash(f'{w.location} successfully added', 'success')
+#         return redirect(url_for('warehouses_new'))
+
+#     else:
+#         flash(f'{w.errors[0]}', 'danger')
+#         return redirect(url_for('warehouses_new'))
 
 
 if __name__ == '__main__':
