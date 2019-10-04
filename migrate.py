b828ae1eb37ee9b3e40f18031fcf9b562d88029f
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-10-05 03:53:47 +0800

    modified migrate.py

--- migrate.py
+++ migrate.py
@@ -10,7 +10,7 @@ if not os.getenv('FLASK_ENV') == 'production':
 
 import peeweedbevolve
 from models import *
-from models import db
+from models.base_model import db
 
 print("Running Migration")
 if os.getenv('FLASK_ENV') == 'production':
