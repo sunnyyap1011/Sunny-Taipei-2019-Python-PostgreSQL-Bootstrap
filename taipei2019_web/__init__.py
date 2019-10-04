b220c0568ff5b53255fad27f481b82ca0d3e625b
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-10-05 06:09:29 +0800

    Added favicon'

--- taipei2019_web/__init__.py
+++ taipei2019_web/__init__.py
@@ -1,8 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_from_directory
 from taipei2019_web.blueprints.restaurants.views import restaurants_blueprint
 from flask_assets import Environment, Bundle
-from .util.assets import bundles  
+from .util.assets import bundles
+import os
 
 assets = Environment(app)
 assets.register(bundles)
@@ -13,3 +14,9 @@ app.register_blueprint(restaurants_blueprint, url_prefix="/restaurants")
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'image'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
