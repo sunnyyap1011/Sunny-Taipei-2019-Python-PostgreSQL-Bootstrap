98bacc3d01b9ad11029a509de21433fab10f218b
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-10-11 23:29:34 +0800

    Remove bootstrap js script at the bottom of _layout.html, wrongly included twice

---
+++ taipei2019_web/static/js/main.js
@@ -0,0 +1,5 @@
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+        $(this).collapse('hide');
+    }
+});
--- taipei2019_web/util/assets.py
+++ taipei2019_web/util/assets.py
@@ -6,7 +6,7 @@ bundles = {
         'js/vendor/jquery-3.3.1.js',
         'js/vendor/popper.js',
         'js/vendor/bootstrap-4.1.3.js',
-        # 'js/main.js',
+        'js/main.js',
         filters='jsmin',
         output='gen/home.%(version)s.js'),
 
